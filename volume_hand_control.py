cf9d3b7198275f0d14a2d8bb90b755b3e61a34be
Iliaromanov <iliaromanov033@gmail.com>
2021-07-14 21:47:23 -0400

    controling volume with gestures

--- volume_hand_control.py
+++ volume_hand_control.py
@@ -2,17 +2,28 @@ import cv2
 import mediapipe
 import numpy as np
 import time
+import math
 
 import HandTrackingModule as htm  # import created hand tracking module
 
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
 
 CAP_WIDTH, CAP_HEIGHT = 1000, 750
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+
+MIN_VOLUME, MAX_VOLUME = volume.GetVolumeRange()[0], volume.GetVolumeRange()[1]
 
 cap = cv2.VideoCapture(0)
 cap.set(3, CAP_WIDTH)  # id 3 => capture window width
 cap.set(4, CAP_HEIGHT)  # id 4 => capture window height
 
-detector = htm.HandDetector(max_num_hands=1, min_detection_confidence=0.65)
+detector = htm.HandDetector(max_num_hands=1, min_detection_confidence=0.8)
+
 
 prev_time = 0  # set initial time for fps tracking
 
@@ -24,10 +35,29 @@ while True:
     if landmark_list:
         thumb_x, thumb_y = landmark_list[4][1], landmark_list[4][2]
         index_x, index_y = landmark_list[8][1], landmark_list[8][2]
+        center_x, center_y = (thumb_x + index_x) // 2, (thumb_y + index_y) // 2
 
         # Draw circles over target finger tips
-        cv2.circle(img, (thumb_x, thumb_y), 15, (250, 0, 0), cv2.FILLED)
-        cv2.circle(img, (index_x, index_y), 15, (250, 0, 0), cv2.FILLED)
+        cv2.circle(img, (thumb_x, thumb_y), 12, (250, 0, 0), cv2.FILLED)
+        cv2.circle(img, (index_x, index_y), 12, (250, 0, 0), cv2.FILLED)
+        # Draw circle at center between finger tips
+        cv2.circle(img, (center_x, center_y), 9, (250, 0, 0), cv2.FILLED)
+
+        # Draw line between target finger tips
+        cv2.line(img, (thumb_x, thumb_y), (index_x, index_y), (250, 0, 50), 3)
+
+        # Calculate distance between the two finger tips
+        dist = math.hypot(thumb_x-index_x, thumb_y-index_y)
+        print(dist)
+        # Assuming max dist between fingers is 425 and min is 15
+        # Transform distance into range of volume
+        vol = np.interp(dist, [50, 200], [MIN_VOLUME, MAX_VOLUME])
+        # Set the volume to the calculated value
+        if MIN_VOLUME < vol and vol < MAX_VOLUME:  # just to be sure
+            volume.SetMasterVolumeLevel(vol, None)
+
+        vol_percent = np.interp(vol, [MIN_VOLUME, MAX_VOLUME], [0, 100])
+        cv2.putText(img, f"Vol: {int(vol_percent)}%", (40, 80), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
 
     # Tracking fps
     cur_time = time.time()
@@ -38,4 +68,4 @@ while True:
 
     # Displaying video frame
     cv2.imshow("Volume Hand Control", img)
-    cv2.waitKey(1)
+    cv2.waitKey(5)
