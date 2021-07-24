8fe4cbe38ed1a6158dab853a4ff1fc92edcb289e
Iliaromanov <iliaromanov033@gmail.com>
2021-07-24 14:25:24 -0400

    added power button toggle by mouse click functionality

--- main.py
+++ main.py
@@ -31,7 +31,8 @@ interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
 volume = cast(interface, POINTER(IAudioEndpointVolume))
 ###################################################################
 
-click_x, click_y = None, None
+# When true the program is "on", when False program is off
+power_button_state = True
 
 def main():
     # Set up for video capture window
@@ -45,7 +46,7 @@ def main():
 
     prev_time = 0  # set initial time for fps tracking
 
-    power_button_img = cv2.imread(r'images\power-button.png', cv2.IMREAD_UNCHANGED)
+    power_button_img = cv2.imread(r'images\power-button.png')
     power_button_img = cv2.resize(power_button_img, dsize=(100, 100))
 
     while True:
@@ -53,74 +54,77 @@ def main():
 
         img[0:100, 0:100] = power_button_img
 
-        img = detector.find_hands(img)
-        landmark_list = detector.find_positions(img)
+        if power_button_state:
+            img = detector.find_hands(img)
+            landmark_list = detector.find_positions(img)
 
-        vol_percent = volume.GetMasterVolumeLevelScalar()
+            vol_percent = volume.GetMasterVolumeLevelScalar()
 
-        #volume_bar_x = VOL_BAR_X1 + round((VOL_BAR_X2 - VOL_BAR_X1) * vol_percent)
-        volume_bar_y = VOL_BAR_Y2 - round((VOL_BAR_Y2 - VOL_BAR_Y1) * vol_percent)
+            #volume_bar_x = VOL_BAR_X1 + round((VOL_BAR_X2 - VOL_BAR_X1) * vol_percent)
+            volume_bar_y = VOL_BAR_Y2 - round((VOL_BAR_Y2 - VOL_BAR_Y1) * vol_percent)
 
-        if landmark_list:
-            thumb_x, thumb_y = landmark_list[4][1], landmark_list[4][2]
-            index_x, index_y = landmark_list[8][1], landmark_list[8][2]
-            center_x, center_y = (thumb_x + index_x) // 2, (thumb_y + index_y) // 2
+            if landmark_list:
+                thumb_x, thumb_y = landmark_list[4][1], landmark_list[4][2]
+                index_x, index_y = landmark_list[8][1], landmark_list[8][2]
+                center_x, center_y = (thumb_x + index_x) // 2, (thumb_y + index_y) // 2
 
-            fingers_up = htm.HandDetector.fingers_up(landmark_list)
+                fingers_up = htm.HandDetector.fingers_up(landmark_list)
 
-            # Mouse control
-            if index_x in range(MOUSE_CTRL_WINDOW_X1, MOUSE_CTRL_WINDOW_X2) and \
-               index_y in range(MOUSE_CTRL_WINDOW_Y1, MOUSE_CTRL_WINDOW_Y2):
-                cv2.rectangle(img, (MOUSE_CTRL_WINDOW_X1, MOUSE_CTRL_WINDOW_Y1),
-                              (MOUSE_CTRL_WINDOW_X2, MOUSE_CTRL_WINDOW_Y2), (0, 255, 0), 3)
-                # NOTE: when in click mode, set mouse location to center circle; NOT index tip
-                # Left click
-                if fingers_up[1] and fingers_up[2]:
-                    dist, img, click = htm.HandDetector.find_distance(img, landmark_list, radius=8)
-                    if click:
-                        autopy.mouse.click()
-                # Right click **thumb up/down only recognized for right hand; could add feature to choose, left or right
-                elif fingers_up[1] and fingers_up[0]:
-                    dist, img, click = htm.HandDetector.find_distance(img, landmark_list, finger_2=0, radius=8)
-                    if click:
-                        pyautogui.click(button="right")
-                # Moving mode
-                elif fingers_up[1]:
-                    new_mouse_x = np.interp(index_x, [MOUSE_CTRL_WINDOW_X1, MOUSE_CTRL_WINDOW_X2], [0, RESOLUTION_W])
-                    new_mouse_y = np.interp(index_y, [MOUSE_CTRL_WINDOW_Y1, MOUSE_CTRL_WINDOW_Y2], [0, RESOLUTION_H])
+                # Mouse control
+                if index_x in range(MOUSE_CTRL_WINDOW_X1, MOUSE_CTRL_WINDOW_X2) and \
+                   index_y in range(MOUSE_CTRL_WINDOW_Y1, MOUSE_CTRL_WINDOW_Y2):
+                    cv2.rectangle(img, (MOUSE_CTRL_WINDOW_X1, MOUSE_CTRL_WINDOW_Y1),
+                                  (MOUSE_CTRL_WINDOW_X2, MOUSE_CTRL_WINDOW_Y2), (0, 255, 0), 3)
+                    # NOTE: when in click mode, set mouse location to center circle; NOT index tip
+                    # Left click
+                    if fingers_up[1] and fingers_up[2]:
+                        dist, img, click = htm.HandDetector.find_distance(img, landmark_list, radius=8)
+                        if click:
+                            autopy.mouse.click()
+                    elif fingers_up[1] and fingers_up[0]:
+                        dist, img, click = htm.HandDetector.find_distance(img, landmark_list, finger_2=0, radius=8)
+                        if click:
+                            pyautogui.click(button="right")
+                    # Moving mode
+                    elif fingers_up[1]:
+                        new_mouse_x = np.interp(index_x, [MOUSE_CTRL_WINDOW_X1,
+                                                          MOUSE_CTRL_WINDOW_X2], [0, RESOLUTION_W])
+                        new_mouse_y = np.interp(index_y, [MOUSE_CTRL_WINDOW_Y1,
+                                                          MOUSE_CTRL_WINDOW_Y2], [0, RESOLUTION_H])
 
-                    mouse_x = prev_mouse_x + (new_mouse_x - prev_mouse_x) / SMOOTHING
-                    mouse_y = prev_mouse_y + (new_mouse_y - prev_mouse_y) / SMOOTHING
-                    autopy.mouse.move(mouse_x, mouse_y)
+                        mouse_x = prev_mouse_x + (new_mouse_x - prev_mouse_x) / SMOOTHING
+                        mouse_y = prev_mouse_y + (new_mouse_y - prev_mouse_y) / SMOOTHING
+                        autopy.mouse.move(mouse_x, mouse_y)
 
-                    prev_mouse_x, prev_mouse_y = mouse_x, mouse_y
+                        prev_mouse_x, prev_mouse_y = mouse_x, mouse_y
 
-            if index_x in range(VOL_BAR_X1, VOL_BAR_X2) and index_y in range(VOL_BAR_Y1, VOL_BAR_Y2) and fingers_up[1]:
-                cv2.rectangle(img, (VOL_BAR_X1, VOL_BAR_Y1), (VOL_BAR_X2, VOL_BAR_Y2), (0, 255, 0), 3)
-                # volume_bar_x = index_x
-                # vol_percent = (volume_bar_x - VOL_BAR_X1) / (VOL_BAR_X2 - VOL_BAR_X1)
-                # Vertical volume bar
-                volume_bar_y = index_y
-                vol_percent = (VOL_BAR_Y2 - volume_bar_y) / (VOL_BAR_Y2 - VOL_BAR_Y1)
-                volume.SetMasterVolumeLevelScalar(vol_percent, None)
+                if index_x in range(VOL_BAR_X1, VOL_BAR_X2) and \
+                   index_y in range(VOL_BAR_Y1, VOL_BAR_Y2) and fingers_up[1]:
+                    cv2.rectangle(img, (VOL_BAR_X1, VOL_BAR_Y1), (VOL_BAR_X2, VOL_BAR_Y2), (0, 255, 0), 3)
+                    # volume_bar_x = index_x
+                    # vol_percent = (volume_bar_x - VOL_BAR_X1) / (VOL_BAR_X2 - VOL_BAR_X1)
+                    # Vertical volume bar
+                    volume_bar_y = index_y
+                    vol_percent = (VOL_BAR_Y2 - volume_bar_y) / (VOL_BAR_Y2 - VOL_BAR_Y1)
+                    volume.SetMasterVolumeLevelScalar(vol_percent, None)
 
-        # # Drawing Horizontal volume control bar
-        # cv2.rectangle(img, (VOL_BAR_X1, VOL_BAR_Y1), (VOL_BAR_X2, VOL_BAR_Y2), (250, 0, 0), 1)
-        # cv2.rectangle(img, (VOL_BAR_X1, VOL_BAR_Y1), (volume_bar_x, VOL_BAR_Y2), (250, 0, 0), cv2.FILLED)
-        # cv2.line(img, (volume_bar_x, VOL_BAR_Y1 - 5), (volume_bar_x, VOL_BAR_Y2 + 5), (255, 0, 25), 5)
+            # # Drawing Horizontal volume control bar
+            # cv2.rectangle(img, (VOL_BAR_X1, VOL_BAR_Y1), (VOL_BAR_X2, VOL_BAR_Y2), (250, 0, 0), 1)
+            # cv2.rectangle(img, (VOL_BAR_X1, VOL_BAR_Y1), (volume_bar_x, VOL_BAR_Y2), (250, 0, 0), cv2.FILLED)
+            # cv2.line(img, (volume_bar_x, VOL_BAR_Y1 - 5), (volume_bar_x, VOL_BAR_Y2 + 5), (255, 0, 25), 5)
 
-        # Vertical volume bar
-        cv2.rectangle(img, (VOL_BAR_X1, VOL_BAR_Y1), (VOL_BAR_X2, VOL_BAR_Y2), (250, 0, 0), 1)
-        cv2.rectangle(img, (VOL_BAR_X1, volume_bar_y), (VOL_BAR_X2, VOL_BAR_Y2), (250, 0, 0), cv2.FILLED)
-        cv2.line(img, (VOL_BAR_X1 - 5, volume_bar_y), (VOL_BAR_X2 + 5, volume_bar_y), (255, 0, 25), 5)
+            # Vertical volume bar
+            cv2.rectangle(img, (VOL_BAR_X1, VOL_BAR_Y1), (VOL_BAR_X2, VOL_BAR_Y2), (250, 0, 0), 1)
+            cv2.rectangle(img, (VOL_BAR_X1, volume_bar_y), (VOL_BAR_X2, VOL_BAR_Y2), (250, 0, 0), cv2.FILLED)
+            cv2.line(img, (VOL_BAR_X1 - 5, volume_bar_y), (VOL_BAR_X2 + 5, volume_bar_y), (255, 0, 25), 5)
 
-        # Drawing mouse controller box
-        cv2.rectangle(img, (MOUSE_CTRL_WINDOW_X1, MOUSE_CTRL_WINDOW_Y1),
-                      (MOUSE_CTRL_WINDOW_X2, MOUSE_CTRL_WINDOW_Y2), (250, 0, 0), 1)
+            # Drawing mouse controller box
+            cv2.rectangle(img, (MOUSE_CTRL_WINDOW_X1, MOUSE_CTRL_WINDOW_Y1),
+                          (MOUSE_CTRL_WINDOW_X2, MOUSE_CTRL_WINDOW_Y2), (250, 0, 0), 1)
 
-        # Displaying Volume Level Percentage
-        cv2.putText(img, f"Vol: {round(vol_percent * 100)}%", (VOL_BAR_X1, CAP_HEIGHT-285),
-                    cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
+            # Displaying Volume Level Percentage
+            cv2.putText(img, f"Vol: {round(vol_percent * 100)}%", (VOL_BAR_X1, CAP_HEIGHT-285),
+                        cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
 
         # Tracking fps
         cur_time = time.time()
@@ -134,18 +138,19 @@ def main():
         cv2.waitKey(1)
 
         # Detect mouse clicks
-        cv2.setMouseCallback('Hand Gesture Controller', detect_mouse_click)
+        cv2.setMouseCallback('Hand Gesture Controller', power_button_toggle)
 
 
-def detect_mouse_click(event, x, y, flags, params):
+def power_button_toggle(event, x, y, flags, params):
     """
     Returns the x and y coordinates of the mouse when left clicked
       within the cv2 capture img window.
     """
-    global click_x, click_y
+    global power_button_state
 
     if event == cv2.EVENT_LBUTTONDOWN:
-        click_x, click_y = x, y
+        if x in range(POWER_BUTTON_X1, POWER_BUTTON_X2) and y in range(POWER_BUTTON_Y1, POWER_BUTTON_Y2):
+            power_button_state = not power_button_state
 
 
 if __name__ == "__main__":
