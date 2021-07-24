2669374678b1fe42e885c21ee3cf0cd47109ca86
Iliaromanov <iliaromanov033@gmail.com>
2021-07-23 23:04:45 -0400

    vertical volume bar, rendering power button

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 import cv2
-import mediapipe
 import pyautogui
 import autopy
 import numpy as np
 import time
-import math
-
-import HandTrackingModule as htm  # import created hand tracking module
+import os
 
 from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
-# Setting Dimensions and Parameters
+import HandTrackingModule as htm  # import created hand tracking module
+
+
+# Setting Constants
+###################################################################
 RESOLUTION_W, RESOLUTION_H = pyautogui.size()
 print(f"Detected screen resolution: {RESOLUTION_W}x{RESOLUTION_H}")
 CAP_WIDTH, CAP_HEIGHT = 1000, 750
-VOL_BAR_X1, VOL_BAR_X2 = 250, 650  # 45, 95
-VOL_BAR_Y1, VOL_BAR_Y2 = 25, 65    # 150, 450
+VOL_BAR_X1, VOL_BAR_X2 = 15, 75  # 250, 650
+VOL_BAR_Y1, VOL_BAR_Y2 = 150, CAP_HEIGHT-360  #25, 65
 MOUSE_CTRL_WINDOW_X1, MOUSE_CTRL_WINDOW_X2 = 200, CAP_WIDTH - 100
-MOUSE_CTRL_WINDOW_Y1, MOUSE_CTRL_WINDOW_Y2 = 90, CAP_HEIGHT-300
+MOUSE_CTRL_WINDOW_Y1, MOUSE_CTRL_WINDOW_Y2 = 35, VOL_BAR_Y2
 SMOOTHING = 5  # Determines mouse movement sensitivity
+###################################################################
 
 # Retrieving computer audio setup information
 devices = AudioUtilities.GetSpeakers()
@@ -38,17 +40,21 @@ prev_mouse_x, prev_mouse_y = 0, 0  # pyautogui.position()
 
 prev_time = 0  # set initial time for fps tracking
 
+power_button_img = cv2.imread(r'images\power-button.png')
+power_button_img = cv2.resize(power_button_img, dsize=(100, 100))
+
 while True:
     success, img = cap.read()
+
+    img[0:100, 0:100] = power_button_img
+
     img = detector.find_hands(img)
-    # img = cv2.flip(img, 1)
     landmark_list = detector.find_positions(img)
 
     vol_percent = volume.GetMasterVolumeLevelScalar()
-    cv2.putText(img, f"Vol: {round(vol_percent * 100)}%", (40, 80), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
 
-    volume_bar_x = VOL_BAR_X1 + round((VOL_BAR_X2 - VOL_BAR_X1) * vol_percent)
-    # volume_bar_y = VOL_BAR_Y2 - round((VOL_BAR_Y2 - VOL_BAR_Y1) * vol_percent)
+    #volume_bar_x = VOL_BAR_X1 + round((VOL_BAR_X2 - VOL_BAR_X1) * vol_percent)
+    volume_bar_y = VOL_BAR_Y2 - round((VOL_BAR_Y2 - VOL_BAR_Y1) * vol_percent)
 
     if landmark_list:
         thumb_x, thumb_y = landmark_list[4][1], landmark_list[4][2]
@@ -84,36 +90,39 @@ while True:
 
                 prev_mouse_x, prev_mouse_y = mouse_x, mouse_y
 
-        if index_x in range(VOL_BAR_X1, VOL_BAR_X2) and index_y in range(VOL_BAR_Y1, VOL_BAR_Y2):
+        if index_x in range(VOL_BAR_X1, VOL_BAR_X2) and index_y in range(VOL_BAR_Y1, VOL_BAR_Y2) and fingers_up[1]:
             cv2.rectangle(img, (VOL_BAR_X1, VOL_BAR_Y1), (VOL_BAR_X2, VOL_BAR_Y2), (0, 255, 0), 3)
-            volume_bar_x = index_x
-            vol_percent = (volume_bar_x - VOL_BAR_X1) / (VOL_BAR_X2 - VOL_BAR_X1)
+            # volume_bar_x = index_x
+            # vol_percent = (volume_bar_x - VOL_BAR_X1) / (VOL_BAR_X2 - VOL_BAR_X1)
             # Vertical volume bar
-            # volume_bar_y = index_y
-            # vol_percent = (VOL_BAR_Y2 - volume_bar_y) / (VOL_BAR_Y2 - VOL_BAR_Y1)
+            volume_bar_y = index_y
+            vol_percent = (VOL_BAR_Y2 - volume_bar_y) / (VOL_BAR_Y2 - VOL_BAR_Y1)
             volume.SetMasterVolumeLevelScalar(vol_percent, None)
 
-    # Drawing Horizontal volume control bar
-    cv2.rectangle(img, (VOL_BAR_X1, VOL_BAR_Y1), (VOL_BAR_X2, VOL_BAR_Y2), (250, 0, 0), 1)
-    cv2.rectangle(img, (VOL_BAR_X1, VOL_BAR_Y1), (volume_bar_x, VOL_BAR_Y2), (250, 0, 0), cv2.FILLED)
-    cv2.line(img, (volume_bar_x, VOL_BAR_Y1 - 5), (volume_bar_x, VOL_BAR_Y2 + 5), (255, 0, 25), 5)
+    # # Drawing Horizontal volume control bar
+    # cv2.rectangle(img, (VOL_BAR_X1, VOL_BAR_Y1), (VOL_BAR_X2, VOL_BAR_Y2), (250, 0, 0), 1)
+    # cv2.rectangle(img, (VOL_BAR_X1, VOL_BAR_Y1), (volume_bar_x, VOL_BAR_Y2), (250, 0, 0), cv2.FILLED)
+    # cv2.line(img, (volume_bar_x, VOL_BAR_Y1 - 5), (volume_bar_x, VOL_BAR_Y2 + 5), (255, 0, 25), 5)
 
     # Vertical volume bar
-    # cv2.rectangle(img, (VOL_BAR_X1, VOL_BAR_Y1), (VOL_BAR_X2, VOL_BAR_Y2), (250, 0, 0), 1)
-    # cv2.rectangle(img, (VOL_BAR_X1, volume_bar_y), (VOL_BAR_X2, VOL_BAR_Y2), (250, 0, 0), cv2.FILLED)
-    # cv2.line(img, (VOL_BAR_X1 - 5, volume_bar_y), (VOL_BAR_X2 + 5, volume_bar_y), (255, 0, 25), 5)
+    cv2.rectangle(img, (VOL_BAR_X1, VOL_BAR_Y1), (VOL_BAR_X2, VOL_BAR_Y2), (250, 0, 0), 1)
+    cv2.rectangle(img, (VOL_BAR_X1, volume_bar_y), (VOL_BAR_X2, VOL_BAR_Y2), (250, 0, 0), cv2.FILLED)
+    cv2.line(img, (VOL_BAR_X1 - 5, volume_bar_y), (VOL_BAR_X2 + 5, volume_bar_y), (255, 0, 25), 5)
 
     # Drawing mouse controller box
     cv2.rectangle(img, (MOUSE_CTRL_WINDOW_X1, MOUSE_CTRL_WINDOW_Y1),
                   (MOUSE_CTRL_WINDOW_X2, MOUSE_CTRL_WINDOW_Y2), (250, 0, 0), 1)
+
+    # Displaying Volume Level Percentage
+    cv2.putText(img, f"Vol: {round(vol_percent * 100)}%", (VOL_BAR_X1, CAP_HEIGHT-285), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
 
     # Tracking fps
     cur_time = time.time()
     fps = 1/(cur_time - prev_time)
     prev_time = cur_time
     # Displaying FPS
-    cv2.putText(img, f"FPS: {int(fps)}", (40, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
+    cv2.putText(img, f"FPS: {int(fps)}", (VOL_BAR_X1, CAP_HEIGHT-255), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
 
     # Displaying video frame
-    cv2.imshow("Volume Hand Control", img)
+    cv2.imshow("Hand Gesture Controller", img)
     cv2.waitKey(1)
