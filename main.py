4f03508ab59aafe85b5679cebd4e1039ea632aa9
Iliaromanov <iliaromanov033@gmail.com>
2021-07-24 13:28:13 -0400

    added mouse callback function

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ RESOLUTION_W, RESOLUTION_H = pyautogui.size()
 print(f"Detected screen resolution: {RESOLUTION_W}x{RESOLUTION_H}")
 CAP_WIDTH, CAP_HEIGHT = 1000, 750
 VOL_BAR_X1, VOL_BAR_X2 = 15, 75  # 250, 650
-VOL_BAR_Y1, VOL_BAR_Y2 = 150, CAP_HEIGHT-360  #25, 65
-MOUSE_CTRL_WINDOW_X1, MOUSE_CTRL_WINDOW_X2 = 200, CAP_WIDTH - 100
+VOL_BAR_Y1, VOL_BAR_Y2 = 150, CAP_HEIGHT-360  # 25, 65
+MOUSE_CTRL_WINDOW_X1, MOUSE_CTRL_WINDOW_X2 = 150, CAP_WIDTH - 100
 MOUSE_CTRL_WINDOW_Y1, MOUSE_CTRL_WINDOW_Y2 = 35, VOL_BAR_Y2
 SMOOTHING = 5  # Determines mouse movement sensitivity
 ###################################################################
@@ -42,6 +42,25 @@ prev_time = 0  # set initial time for fps tracking
 
 power_button_img = cv2.imread(r'images\power-button.png')
 power_button_img = cv2.resize(power_button_img, dsize=(100, 100))
+
+click_x, click_y = None, None
+
+def detect_mouse_click(event, x, y, flags, params):
+    """
+    Returns the x and y coordinatesof the mouse when left clicked
+      relative to the cv2 capture img.
+    :param event:
+    :param x:
+    :param y:
+    :param flags:
+    :param params:
+    :return:
+    """
+    global click_x, click_y
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        click_x, click_y = x, y
+
 
 while True:
     success, img = cap.read()
@@ -126,3 +145,9 @@ while True:
     # Displaying video frame
     cv2.imshow("Hand Gesture Controller", img)
     cv2.waitKey(1)
+
+    # Detect mouse clicks
+    cv2.setMouseCallback('Hand Gesture Controller', detect_mouse_click)
+    # click_x, click_y = click_coordinates
+    if click_x and click_y:
+        print((click_x, click_y))
